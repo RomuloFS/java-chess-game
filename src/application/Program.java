e56ddded62a405300e03d56b4e3a758b76d7a2ef
RomuloFS <romulosousa865@gmail.com>
2022-08-04 20:25:05 -0300

    Movendo as peças

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,29 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
 		ChessMatch match = new ChessMatch();
-		UI.printBoard(match.getPieces());
 		
+		while(true) {
+			UI.printBoard(match.getPieces());
+			System.out.println();
+			System.out.println("Origem: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.println("Destino: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = match.performChessMove(source, target);
+		}
 	}
 }
