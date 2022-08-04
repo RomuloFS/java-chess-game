d18d7c54200e5f29af420e14fbc7a3c0334cb5ae
RomuloFS <romulosousa865@gmail.com>
2022-08-04 20:47:43 -0300

    Movimentos possiveis de uma peça

--- src/application/Program.java
+++ src/application/Program.java
@@ -26,7 +26,7 @@ public class Program {
 				System.out.println();
 				System.out.println("Destino: ");
 				ChessPosition target = UI.readChessPosition(sc);
-				
+				 
 				ChessPiece capturedPiece = match.performChessMove(source, target);
 			} catch(ChessException ex) {
 				System.out.println(ex.getMessage());
