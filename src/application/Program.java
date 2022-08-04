e2f50ff34bac4007298c90a0428ab990af1cb649
RomuloFS <romulosousa865@gmail.com>
2022-08-03 21:19:59 -0300

    Colocando as peças no tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,9 +5,9 @@ import chess.ChessMatch;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		ChessMatch match = new ChessMatch();
 		UI.printBoard(match.getPieces());
-
+		
 	}
 }
