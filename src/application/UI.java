42372d1ceb7fcf63074f654a0865301df33c67b2
RomuloFS <romulosousa865@gmail.com>
2022-08-11 21:26:54 -0300

    Lógica do CHECKMATE

--- src/application/UI.java
+++ src/application/UI.java
@@ -78,10 +78,16 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turno: " + chessMatch.getTurn());
-		System.out.println("Esparando o jogador: " + chessMatch.getCurrentPlayer());
-		
-		if(chessMatch.getCheck()) {
-			System.out.println("CHECK!");
+
+		if(!chessMatch.getCheckMate()) {
+			System.out.println("Esparando o jogador: " + chessMatch.getCurrentPlayer());
+			
+			if(chessMatch.getCheck()) {
+				System.out.println("CHECK!");
+			}
+		} else {
+			System.out.println("CHECKMATE!!!!!!!!!!!!!");
+			System.out.println("VENCEDOR: " + chessMatch.getCurrentPlayer());
 		}
 	}
 	
